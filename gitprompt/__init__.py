e3940e664e7b7a48c9034b22099f479a8e5b2519
shakefu <shakefu@gmail.com>
2014-02-03 14:30:32 -0800

    Leverage 256 colors for my personal prompt. Make formatting context more flexible.

--- gitprompt/__init__.py
+++ gitprompt/__init__.py
@@ -39,7 +39,7 @@ def default_prompt(f):
     return prompt
 
 
-def jakes_prompt(f):
+def shakefus_prompt(f):
     console_width = int(os.environ.get('COLUMNS'))
     decor = 10
     term = blessings.Terminal(force_styling=True)
@@ -59,11 +59,11 @@ def jakes_prompt(f):
     padding = padding[width - 1:]
     if f.venv:
         venv = "═" * (width - len(" (" + str(f.venv) + ") "))
-        venv = "{c.blue}" + venv + "{c.normal}"
+        venv = "{line_color}" + venv + "{c.normal}"
         venv = (" {c.yellow}(" + venv_color + "{venv}{c.yellow}){c.normal} "
                 + venv)
     else:
-        venv = "{c.blue}═{c.normal}" * width
+        venv = "{line_color}═{c.normal}" * width
 
     # Repo/Branch
     width = 30
@@ -71,15 +71,15 @@ def jakes_prompt(f):
     if f.git.repo:
         git = ' %s/%s ' % (f.git.repo, f.git.branch) + '== '
         git = "═" * (width - len(git))
-        git = "{c.blue}" + git + "{c.normal}"
+        git = "{line_color}" + git + "{c.normal}"
         git = (git + " " + repo_color
                 + "{git.repo}{c.yellow}/{c.normal}{git.branch} ")
         if f.git.all_changes:
-            git += "{c.blue}══{c.normal} "
+            git += "{line_color}══{c.normal} "
         else:
-            git += "{c.blue}═══{c.normal}"
+            git += "{line_color}═══{c.normal}"
     else:
-        git = "{c.blue}═{c.normal}" * width
+        git = "{line_color}═{c.normal}" * width
 
     # Changes
     width = 12
@@ -87,17 +87,17 @@ def jakes_prompt(f):
     if f.git.all_changes:
         changes = str(f.git.all_changes).format(c=no_color) + " "
         changes = "═" * (width - len(changes))
-        changes = str(f.git.all_changes) + " {c.blue}" + changes + "{c.normal}"
+        changes = str(f.git.all_changes) + " {line_color}" + changes + "{c.normal}"
     else:
-        changes = "{c.blue}═{c.normal}" * width
+        changes = "{line_color}═{c.normal}" * width
 
     # User@Host
     padding = padding[len('%s@%s' % (f.user, f.host)):]
     login = "{c.cyan}{user}{c.yellow}@{c.normal}{host}"
 
     padding = "═" * (len(padding) - 1) + " "
-    middle = venv + git + changes + "{c.blue}" + padding + "{c.normal}" + login
-    prompt =  "\r {c.blue}╒══{c.normal}" + middle + " {c.blue}══──{c.normal}\n"
+    middle = venv + git + changes + "{line_color}" + padding + "{c.normal}" + login
+    prompt =  "\r {line_color}╒══{c.normal}" + middle + " {line_color}══──{c.normal}\n"
 
     # Git status
     padding = " " * (console_width - decor)
@@ -108,7 +108,7 @@ def jakes_prompt(f):
         for line in status.split('\n'):
             if not line.strip():
                 continue
-            prompt += " {c.blue}│{c.normal}" + " " * git_offset + line + "\n"
+            prompt += " {line_color}│{c.normal}" + " " * git_offset + line + "\n"
 
     # Bottom line
     padding = " " * (console_width - decor)
@@ -126,20 +126,20 @@ def jakes_prompt(f):
         cwd = cwd + '{c.normal}'
     else:
         cwd = '{c.red}/{c.normal}'
-    cwd += " {c.blue}══──{c.normal}"
+    cwd += " {line_color}══──{c.normal}"
 
     timestamp = str(f.time)
     padding = padding[len("--== " + timestamp):]
     timestamp = timestamp.replace(':', '{c.yellow}:{c.bold_black}')
     timestamp = '{c.bold_black}' + timestamp + '{c.normal}'
 
-    middle = cwd + padding + "{c.blue}──══ {c.normal}" + timestamp
-    prompt += " {c.blue}╘══{c.normal} " + middle + " {c.blue}══──{c.normal}"
+    middle = cwd + padding + "{line_color}──══ {c.normal}" + timestamp
+    prompt += " {line_color}╘══{c.normal} " + middle + " {line_color}══──{c.normal}"
 
     return prompt
 
 
-pyconfig.set('gitprompt.prompt', jakes_prompt)
+pyconfig.set('gitprompt.prompt', shakefus_prompt)
 
 
 def get_prompt(args):
@@ -182,8 +182,14 @@ def main():
 
     # Format our prompt, woot!
     try:
-        prompt = prompt.format(c=term, **fields.get_fields())
-        prompt = prompt.format(c=term)
+        format_context = pyconfig.get('gitprompt.custom_context', {})
+        format_context.update(fields.get_fields())
+        format_context.setdefault('line_color', term.color(105))
+        format_context.setdefault('c', term)
+
+        prompt = prompt.format(**format_context)
+        # This isn't a dupe, we format twice, since fields can have colors
+        prompt = prompt.format(**format_context)
     except:
         if args.debug:
             raise
